0471ae6681e827f8347937d342562e801bee7f9c
EvelynXD <91973757+EvelynXD@users.noreply.github.com>
2024-04-05 10:20:49 +0700

    Update SessionProvider.js

--- src/app/SessionProvider.js
+++ src/app/SessionProvider.js
@@ -31,7 +31,7 @@ export function AuthProvider({ children, session }) {
             </SkeletonTheme>
             <ProgressBar
                 height="3px"
-                color="#CA1313"
+                color="#00c8ff"
                 options={{ showSpinner: true }}
             // shallowRouting // by enabling this progressbar does not show on query params change
             />
